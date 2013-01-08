cec08e15cc921451a959253c004614efa50289b2
prokla <juscelino@redel.com.br>
2013-01-08 20:58:44 -0200

    localidade

--- application/controllers/EventosController.php
+++ application/controllers/EventosController.php
@@ -6,15 +6,18 @@ class EventosController extends Zend_Controller_Action
     public function init()
     {
     	
-    	
     	$sess = new Zend_Session_Namespace('City');
-  	
-     	if($sess->cityId > 0) {
+     	if(($sess->cityId) > 0 || $sess->cityId == null || !isset($sess->cityId) || $_SESSION['City']['id'] == null) {
+     		
+     		//print_r($sess);
+     		
      		$this->cityId = $sess->id;
      		$this->cityNome = $sess->nome;
      		$this->view->cityNome = $this->cityNome;
     	}else {
     		return $this->_forward('index', 'localidade');
+    		header("Location: /localidade");
+    		exit;
     	}
     }
     
@@ -22,11 +25,18 @@ class EventosController extends Zend_Controller_Action
 
     public function indexAction()
     {
+    	
+    	if($this->cityId == null)
+    	{
+    		return $this->_forward('index', 'localidade');
+    	}else{
+    	
     	$eventos = new Application_Model_DbTable_Eventos();
     	$this->view->eventos = $eventos->findByCity($this->cityId);
     	
     	$eventosTable = new Application_Model_DbTable_Eventos();
     	$this->view->destaques = $eventosTable->findDestaques();
+   		 }
     }
 
     
