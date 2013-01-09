119d8ce35c3a64a4df094bb3e64def8c803ed255
prokla <juscelino@redel.com.br>
2013-01-09 18:02:58 -0200

    cidades no evento

--- application/controllers/EventoController.php
+++ application/controllers/EventoController.php
@@ -6,6 +6,10 @@ class EventoController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+    	$sess = new Zend_Session_Namespace('City');
+    	$this->cityId = $sess->id;
+    	$this->cityNome = $sess->nome;
+    	$this->view->cityNome = $this->cityNome;
     }
 
     
