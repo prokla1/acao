f563a5dd01afb7a2e4b8eeb918589abc9b51d78c
prokla <juscelino@redel.com.br>
2013-01-10 00:11:06 -0200

    conversao

---
+++ application/models/Conversao.php
@@ -0,0 +1,16 @@
+<?php
+
+class Application_Model_Conversao
+{
+
+	protected $_id;
+	protected $_id_cortesia;
+	protected $_id_evento;
+	protected $_id_usuario;
+	protected $_qtd;
+	protected $_pontos;
+	protected $_reais;
+	protected $_concretizado;
+
+}
+
