d3f20a6a95a1fce4913053533f662f28dae49840
prokla <juscelino@redel.com.br>
2013-01-17 11:42:27 -0200

    partial location

--- application/controllers/EventoController.php
+++ application/controllers/EventoController.php
@@ -5,10 +5,13 @@ class EventoController extends Zend_Controller_Action
 
     public function init()
     {
+    	//$this->_helper->layout->setLayout('parceiro');
+    	/*
     	$sess = new Zend_Session_Namespace('City');
     	$this->cityId = $sess->id;
     	$this->cityNome = $sess->nome;
     	$this->view->cityNome = $this->cityNome;
+    	*/
     }
 
     
