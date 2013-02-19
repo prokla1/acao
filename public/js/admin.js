4a4ec5a518889008887b1718b13f04dea503405d
prokla <juscelino@redel.com.br>
2013-02-18 22:16:52 -0300

    url friendly

---
+++ public/js/admin.js
@@ -0,0 +1,30 @@
+function str_to_slug(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+  
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to   = "aaaaeeeeiiiioooouuuunc------";
+  for (var i=0, l=from.length ; i<l ; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
+
+
+$(document).ready( function() {
+	$("#nome").keypress(function() {
+		$('#url_amigavel').val(str_to_slug($(this).val()));
+	});
+	
+//	$("#nome").stringToSlug({
+//		setEvents: 'keyup keydown blur',
+//		getPut: '#url_amigavel',
+//		space: '-'
+//	});
+});
