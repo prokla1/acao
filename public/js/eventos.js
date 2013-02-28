de19432d0a96b2fb2359be3a5999371ba6a35490
prokla <juscelino@redel.com.br>
2013-02-28 15:12:08 -0300

    without css

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,22 +1,16 @@
-			$(document).ready(function() {
-//				$('#postshare_wrapper').stickyScroll({ container: '.eventos_content' });
-			});
-
-
-
-			$(document).ready(function () {  
-				  var top = $('#postshare_wrapper').offset().top - parseFloat($('#postshare_wrapper').css('marginTop').replace(/auto/, 0));
-				  $(window).scroll(function (event) {
-				    // what the y position of the scroll is
-				    var y = $(this).scrollTop();
-				  
-				    // whether that's below the form
-				    if (y >= top) {
-				      // if so, ad the fixed class
-				      $('#postshare_wrapper').addClass('fixed');
-				    } else {
-				      // otherwise remove it
-				      $('#postshare_wrapper').removeClass('fixed');
-				    }
-				  });
-				});
+$(document).ready(function () {  
+	  var top = $('#postshare_wrapper').offset().top - parseFloat($('#postshare_wrapper').css('marginTop').replace(/auto/, 0));
+	  $(window).scroll(function (event) {
+	    // what the y position of the scroll is
+	    var y = $(this).scrollTop();
+	  
+	    // whether that's below the form
+	    if (y >= top) {
+	      // if so, ad the fixed class
+	      $('#postshare_wrapper').addClass('fixed');
+	    } else {
+	      // otherwise remove it
+	      $('#postshare_wrapper').removeClass('fixed');
+	    }
+	  });
+});
--- public/js/social_functions.js
+++ public/js/social_functions.js
@@ -270,5 +270,5 @@ function shareEvent(event) {
 		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
 		    po.src = 'https://apis.google.com/js/plusone.js';
 		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		  })();
+	  })();
 
