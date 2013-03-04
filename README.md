014dd16776d7f53cb53afb0151cc205f55744611
prokla <juscelino@redel.com.br>
2013-03-04 17:18:50 -0300

    clean

--- README.md
+++
@@ -1,5 +0,0 @@
-acao
-====
-
-
-http://acao.dev/
--- public/js/localidade.js
+++
@@ -1,7 +0,0 @@
-	$(".ajax_content_localidade").slideToggle();
-
-	
-	$('.location_content a').click(function(event){
-		event.preventDefault();
-		console.log('clicou no link  1 localidade.js');
-	});
